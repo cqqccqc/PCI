902cb54799e9d153a6c61439c8225b6fc48460bc
q.chen <q.chen@Ctrip.com>
2015-12-21 21:26:51 +0800

    aaa

--- MakingRecommendations/recommendations.py
+++ MakingRecommendations/recommendations.py
@@ -50,3 +50,34 @@ if __name__ == "__main__":
         return 1/(1 + sqrt(sum_of_squares))
 
     print sim_distance(critics,  'Lisa Rose', 'Gene Seymour')
+
+    def sim_pearson(pref, p1, p2):
+        # 相同评价过的电影列表
+        si = {}
+        for item in pref[p1]:
+            if item in pref[p2]:
+                si[item] = 1
+
+        # 没有则返回0
+        n = len(si)
+        if n == 0:
+            return 1
+
+        # pearson 相关系数
+        # r = (Σxi*yi - (Σxi*Σyi)/n)
+        #     /
+        #     (sqrt(Σxi^2 - (Σxi)^2/n)*(Σyi^2 - (Σyi)^2/n))
+
+        # 求和Σxi
+        sum1 = sum([pref[p1][item] for item in si])
+        sum2 = sum([pref[p2][item] for item in si])
+
+        # 平方和Σxi^2
+        sum1sq = sum([pow(pref[p1][item], 2) for item in si])
+        sum2sq = sum([pow(pref[p2][item], 2) for item in si])
+
+        # 乘积和 Σxi*yi
+        p_sum = sum([pref[p1][item] * pref[p2][item] for item in si])
+
+        num = p_sum - sum1 * sum2 / n
+        den = sqrt((sum1sq-pow(sum1,2)/n) * (sum2sq - pow(sum2,2)/n))
