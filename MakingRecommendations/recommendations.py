95b50e11945b8013db2e242c860dd4cca015390f
q.chen <q.chen@Ctrip.com>
2015-12-28 20:33:53 +0800

    sim

--- MakingRecommendations/recommendations.py
+++ MakingRecommendations/recommendations.py
@@ -30,54 +30,85 @@ critics = {
         'You, Me and Dupree': 2.0,
     }
 }
+
+
+def sim_distance(pref, person1, person2):
+    # 欧式距离
+    # 相同评价过的电影列表
+    si = {}
+    for item in pref[person1]:
+        if item in pref[person2]:
+            si[item] = 1
+
+    # 没有则返回0
+    if len(si) == 0:
+        return 0
+
+    # 所有的差值平方和
+    sum_of_squares = sum([pow(pref[person1][item] - pref[person2][item], 2)
+                          for item in pref[person1] if item in pref[person2]])
+    return 1/(1 + sqrt(sum_of_squares))
+
+
+def sim_pearson(pref, p1, p2):
+    # 皮尔逊相关系数
+    # 相同评价过的电影列表
+    si = {}
+    for item in pref[p1]:
+        if item in pref[p2]:
+            si[item] = 1
+
+    # 没有则返回0
+    n = len(si)
+    if n == 0:
+        return 1
+
+    # pearson 相关系数
+    # r = (Σxi*yi - (Σxi*Σyi)/n)
+    #     /
+    #     (sqrt(Σxi^2 - (Σxi)^2/n)*(Σyi^2 - (Σyi)^2/n))
+
+    # 求和Σxi
+    sum1 = sum([pref[p1][item] for item in si])
+    sum2 = sum([pref[p2][item] for item in si])
+
+    # 平方和Σxi^2
+    sum1sq = sum([pow(pref[p1][item], 2) for item in si])
+    sum2sq = sum([pow(pref[p2][item], 2) for item in si])
+
+    # 乘积和 Σxi*yi
+    p_sum = sum([pref[p1][item] * pref[p2][item] for item in si])
+
+    num = p_sum - sum1 * sum2 / n
+    den = sqrt((sum1sq-pow(sum1, 2)/n) * (sum2sq - pow(sum2, 2)/n))
+    if den == 0:
+        return 0
+
+    r = num / den
+    return r
+
+
+def top_matches(pref, person, n=5, similarity=sim_pearson):
+    # 评论者打分
+    # 打分的列表
+    scores = [(similarity(pref, person, other), other) for other in pref if other != person]
+
+    # 排序
+    scores.sort()
+    # 反转
+    scores.reverse()
+    return scores[0: n]
+
+
+def get_recommendations(pref, person, n=5, similarity=sim_pearson):
+    # 加权值评分
+    pass
+
 if __name__ == "__main__":
     from math import sqrt
 
-    def sim_distance(pref, person1, person2):
-        # 相同评价过的电影列表
-        si = {}
-        for item in pref[person1]:
-            if item in pref[person2]:
-                si[item] = 1
-
-        # 没有则返回0
-        if len(si) == 0:
-            return 0
-
-        # 所有的差值平方和
-        sum_of_squares = sum([pow(pref[person1][item] - pref[person2][item], 2)
-                              for item in pref[person1] if item in pref[person2]])
-        return 1/(1 + sqrt(sum_of_squares))
-
     print sim_distance(critics,  'Lisa Rose', 'Gene Seymour')
 
-    def sim_pearson(pref, p1, p2):
-        # 相同评价过的电影列表
-        si = {}
-        for item in pref[p1]:
-            if item in pref[p2]:
-                si[item] = 1
+    print sim_pearson(critics,  'Lisa Rose', 'Gene Seymour')
 
-        # 没有则返回0
-        n = len(si)
-        if n == 0:
-            return 1
-
-        # pearson 相关系数
-        # r = (Σxi*yi - (Σxi*Σyi)/n)
-        #     /
-        #     (sqrt(Σxi^2 - (Σxi)^2/n)*(Σyi^2 - (Σyi)^2/n))
-
-        # 求和Σxi
-        sum1 = sum([pref[p1][item] for item in si])
-        sum2 = sum([pref[p2][item] for item in si])
-
-        # 平方和Σxi^2
-        sum1sq = sum([pow(pref[p1][item], 2) for item in si])
-        sum2sq = sum([pow(pref[p2][item], 2) for item in si])
-
-        # 乘积和 Σxi*yi
-        p_sum = sum([pref[p1][item] * pref[p2][item] for item in si])
-
-        num = p_sum - sum1 * sum2 / n
-        den = sqrt((sum1sq-pow(sum1,2)/n) * (sum2sq - pow(sum2,2)/n))
+    print top_matches(critics, 'Lisa Rose')
